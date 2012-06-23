136a0e35412f661fdd916e37e70ed2c4b30e9fbd
Nicolai Dahl <ndahl89@gmail.com>
2012-06-23 20:08:56 +0200

    Implemented the rest of the generate methods. Next up variable environment

--- src/main/scala/dk/itu/c/C.scala
+++ src/main/scala/dk/itu/c/C.scala
@@ -27,42 +27,62 @@ furnished to do so, subject to the following conditions:
 package dk.itu.c
 
 trait C {
-  sealed abstract class CAST
-  case class Program (contents: List[TopDec]) extends CAST
   
+  
+  case class Program (contents: List[TopDec])
+  
+  //Top level declaration
   sealed abstract class TopDec
-  case class FunctionDec (returnType: Option[Type], identifier: String, parameters: List[Tuple2[Type, String]], statement: Statement) extends TopDec
+  case class FunctionDec (returnType: Option[Type], identifier: String, parameters: List[(Type, String)], statement: Statement) extends TopDec
   case class VariableDec (variableType: Type, identifier: String) extends TopDec
   
+  //Declarations inside a block
   sealed abstract class BlockDec
   case class BlockStatement (statement: Statement) extends BlockDec
   case class LocalVariable (variableType: Type, identifier: String) extends BlockDec
   
+  //C statements
   sealed abstract class Statement
-  case class Block (contents: List[Statement]) extends Statement
-  case class ExpressionStatement (contents: Expression) extends Statement
+  case class Block (contents: List[BlockDec]) extends Statement
+  case class ExpressionStatement (expr: Expression) extends Statement
   case class If (condition: Expression, trueBranch: Statement, falseBranch: Statement) extends Statement
   case class While (condition: Expression, contents: Statement) extends Statement
   case class Return (returnExpression: Option[Expression]) extends Statement
   
+  //C types
   sealed abstract class Type
-  case class TypeInteger extends Type
-  case class TypeChar extends Type
+  case object TypeInteger extends Type
+  case object TypeChar extends Type
   case class TypeArray (arrayType: Type, length: Option[Integer]) extends Type
   case class TypePointer (pointerType: Type) extends Type
   
+  //C Unary operators
+  sealed abstract class UnaryOp
+  case object UnaryDecrement extends UnaryOp
+  case object UnaryIncrement extends UnaryOp
+
+  //C Binary Operators
+  sealed abstract class BinaryOp
+  case object BinaryPlus extends BinaryOp
+  case object BinaryMinus extends BinaryOp
+  case object BinaryTimes extends BinaryOp
+  case object BinaryDivide extends BinaryOp
+  
+  //C Expressions
   sealed abstract class Expression
-  case class AccessExpr (access: Access) //x    or  *p    or  a[e]
+  case class AccessExpr (access: Access) extends Expression //x    or  *p    or  a[e]
   case class Assign (access: Access, expr: Expression) extends Expression  //x=e  or  *p=e  or  a[e]=e 
   case class Address (access: Access) extends Expression //&x   or  &*p   or  &a[e] 
   case class ConstantInteger (contents: Integer) extends Expression
-  case class UnaryPrim (operator: String, expression: Expression) extends Expression //Unary primitive operator
-  case class BinaryPrim (operator: String, expression1: Expression, expression2: Expression) extends Expression //Binary primitive operator
+  case class UnaryPrim (operator: UnaryOp, expression: Expression) extends Expression //Unary primitive operator
+  case class BinaryPrim (operator: BinaryOp, expression1: Expression, expression2: Expression) extends Expression //Binary primitive operator
   case class SeqAnd (expr1: Expression, expr2: Expression) extends Expression //Sequential and &&
   case class SeqOr (expr1: Expression, expr2: Expression) extends Expression //Sequential or ||
   case class Call (ident: String, args: List[Expression]) extends Expression //Function call f(...)
   case class ConditionExpression (expr1: Expression, expr2: Expression, expr3: Expression) extends Expression //e1 ? e2 : e3
+  case class Cast(expression: Expression, newType: Type) extends Expression //(int) a;
 
+  //C variable access
   sealed abstract class Access
   case class AccessVariable (ident: String) extends Access //Variable access  ident
   case class AccessDeref (expr: Expression) extends Access //Pointer dereferencing  *p
@@ -70,38 +90,105 @@ trait C {
 }
 
 trait Generator extends C {
-  def generate (c: CAST): String =
-    c match {
-      case Program (contents) => contents.map(
-          e=>e match{
-            case function: FunctionDec => generateFunctionDec(function) 
-            case variable: VariableDec => generateVariableDec(variable) 
-       }).mkString("\n")
-  	}
   
-  def evalType(t: Type) : String = 
-    t match {
-      case t1: TypeInteger => "int"
-      case t2: TypeChar => "char"
-      case t3: TypePointer => evalType(t3.pointerType) + "*"
-      case t4: TypeArray => t4.length match {
-        case None => evalType(t4.arrayType) + "[]"
-        case Some(l) => evalType(t4.arrayType) + "[" + l + "]"
-      }
-    }
-    
+  //Main generate function
+  def generate (prog: Program): String =
+    prog.contents.map{
+      case function: FunctionDec => generateFunctionDec(function) 
+      case variable: VariableDec => generateVariableDec(variable) 
+    } mkString "\n"
+  
   def generateFunctionDec(functionDec: FunctionDec): String = {
     val ts = 
       functionDec.returnType match {
         case None => "void"
-        case Some(t) => evalType(t)
+        case Some(t) => generateType(t)
       }
-  
-    ts + " " + functionDec.identifier + "(" + functionDec.parameters.map(tuple => tuple._1 + " " + tuple._2 + ",") + ")" + "{" + functionDec.statement + "}"
+      
+    val returnType = ts
+    val funcName = functionDec.identifier
+
+    val params = (functionDec.parameters.map({
+      case (typ, name) => typ + " " + name
+    }).mkString("(", ", ", ")"))
+
+    val body = "{" + functionDec.statement + "}"
+
+    returnType + " " + funcName + params + body
   }
   
   def generateVariableDec(variableDec: VariableDec): String =
-    evalType(variableDec.variableType) + " " + variableDec.identifier 
+    generateType(variableDec.variableType) + " " + variableDec.identifier 
+  
+    
+  def genereateBlockDec(bd: BlockDec): String =
+    bd match {
+      case BlockStatement(statement) => generateStatement(statement)
+      case LocalVariable(varType, identifier) => generateType(varType) + " " + identifier
+    }
+    
+  def generateStatement(stmt: Statement): String =
+    stmt match {
+      case Block (contents) => ""
+      case ExpressionStatement (expr) => generateExpr(expr) + ";"
+      case If (condition, trueBranch, falseBranch) => 
+        "if(" + generateExpr(condition) + ")\n" +
+        generateStatement(trueBranch) + "\n" +
+        "else\n" +
+        generateStatement(falseBranch) + "\n"
+      case While (condition, contents) => 
+        "while(" + generateExpr(condition) + ")" +
+        generateStatement(contents)
+      case Return (returnExpression) => returnExpression.map(generateExpr).getOrElse("") + ";"
+    }
+    
+  def generateType(t: Type) : String = 
+    t match {
+      case TypeInteger => "int"
+      case TypeChar => "char"
+      case TypePointer(t) => generateType(t) + "*"
+      case TypeArray(t, None) => generateType(t) + "[]"
+      case TypeArray(t, Some(l)) => generateType(t) + "[" + l + "]"
+    }
+  
+  def generateUnaryOp(ope: UnaryOp): String =
+    ope match {
+      case UnaryDecrement => "--"
+      case UnaryIncrement => "++"
+    }
+  
+  def generateBinaryOp(ope: BinaryOp): String =
+    ope match {
+      case BinaryPlus => "+"
+      case BinaryMinus => "-"
+      case BinaryTimes => "*"
+      case BinaryDivide => "/"
+    }
+    
+    
+  def generateExpr(e: Expression): String =
+    e match {
+      case AccessExpr(access) => generateAccess(access)
+      case Assign(access, expr) => generateAccess(access) + "=" + generateExpr(expr)
+      case Address(access) => ""
+      case ConstantInteger(i) => i.toString()
+      case UnaryPrim(operator, expr) => generateUnaryOp(operator) + generateExpr(expr)
+      case BinaryPrim(operator, expr1, expr2) => generateExpr(expr1) + " " + generateBinaryOp(operator) + " " + generateExpr(expr2)
+      case SeqAnd(expr1, expr2) => generateExpr(expr1) + " && " + generateExpr(expr2)
+      case SeqOr(expr1, expr2) => generateExpr(expr1) + " || " + generateExpr(expr2)
+      case Call(identifier, args) => identifier + args.mkString("(", ", ", ")")
+      case ConditionExpression(expr1, expr2, expr3) => generateExpr(expr1) + " ? " + generateExpr(expr2) + " : " + generateExpr(expr3)
+      case Cast(expr, newType) => "(" + generateType(newType) + ") " + generateExpr(expr)
+    }
+  
+  
+  def generateAccess(a: Access): String =
+    a match {
+      case AccessVariable(identifier) => ""
+      case AccessDeref(expr) => "*" + generateExpr(expr)
+      case AccessIndex(access, expr) => generateAccess(access) + "[" + generateExpr(expr) + "]"
+    }
+    
   
   
   /*s match {
@@ -112,3 +199,7 @@ trait Generator extends C {
 object Test extends Generator with App {
   //println(generate(Program(List(VariableDec(TypeInteger(), "kage"), FunctionDec(TypeInteger, "jens", List(Tuple2(TypeInterger, "antal")), )))))
 }
+
+
+
+
