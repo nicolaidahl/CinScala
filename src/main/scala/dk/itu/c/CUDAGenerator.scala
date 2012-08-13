bf3579074f251f06fccb16541623b259ad5bfcea
Nicolai Dahl <ndahl89@gmail.com>
2012-08-13 11:34:16 +0200

    Added more CUDA stuff

--- src/main/scala/dk/itu/c/CUDAGenerator.scala
+++ src/main/scala/dk/itu/c/CUDAGenerator.scala
@@ -8,29 +8,36 @@ trait CUDAGenerator extends CGenerator with CUDAAbstractSyntax {
     generateExternalDeclarations(varEnv, funEnv)(prog.contents)._3
   }
   
-  /*override def generateTopDecs (varEnv: VarEnv, funEnv: FunEnv)(topDecs: List[ExternalDeclaration]): (VarEnv, FunEnv, String) = {
+  override def generateExternalDeclarations (varEnv: VarEnv, funEnv: FunEnv)(topDecs: List[CExternalDeclaration]): (VarEnv, FunEnv, String) = {
     topDecs match {
       case Nil => (varEnv, funEnv, "")
       case head :: tail =>
         head match {
-          case CUDAFunctionDec(functionType, returnType, ident, params, stmtOrDecs) => 
-            val functionTypeStr = generateCUDAFunctionType(functionType)
-            val (funEnv1, str) = generateFunctionDec(varEnv, funEnv, CFunctionDec(returnType, ident, params, stmtOrDecs))
-            val (varEnv1, funEnv2, str1) = generateTopDecs(varEnv, funEnv1)(tail)
+          case CUDAFunctionDec(functionType, typeSpecifier, declarator, declarationList, compoundStmt) => 
+            val functionTypeStr = generateCUDAFunctionQualifier(functionType)
+            val (funEnv1, str) = generateFunctionDec(varEnv, funEnv, CFunctionDec(Some(CDeclarationSpecifiers(None, typeSpecifier, None)), declarator, declarationList, compoundStmt))
+            val (varEnv1, funEnv2, str1) = generateExternalDeclarations(varEnv, funEnv1)(tail)
             
             (varEnv1, funEnv2, functionTypeStr + " " + str + str1)
           case _ =>
-            super.generateTopDecs(varEnv, funEnv)(tail)
+            super.generateExternalDeclarations(varEnv, funEnv)(tail)
         }
       
     }
-  }*/
+  }
   
-  /*def generateCUDAFunctionType(funcType: CUDAFunctionType): String =
+  def generateCUDAFunctionQualifier(funcType: CUDAFunctionQualifier): String =
     funcType match {
-      case GlobalType() => "__global__"
-      case DeviceType() => "__device__"
+      case CUDAGlobalType() => "__global__"
+      case CUDADeviceType() => "__device__"
+      case CUDAHostType() => "__host__"
+      case CUDANoInlineType() => "__noinline__"
     }
-  */
+  
+  override def generatePostfixExpression(varEnv: VarEnv, funEnv: FunEnv)(e: CPostfixExpression): String =
+    e match {
+      case CUDAKernelCall(dg, db, postfixExpression, arguments) =>
+        ""
+    }
   
 }
